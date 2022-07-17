cef642c6820fecb80ae0a4a117a0c46a8fdce9e7
Vaishnav Arora <103623811+er-psycho18@users.noreply.github.com>
2022-07-17 09:52:48 +0530

    Add files via upload

---
+++ reverse_of_string.py
@@ -0,0 +1,19 @@
+# reverse_of_string.py
+
+# Enter the string you want to reverse
+string = "Hello It's er-psycho18 (Vaishnav Arora) here"
+
+# define a empty string here
+reversed_string = " "
+
+#use for loop and give a random name to some var
+for rev in string:
+
+# Now concatenate var with the empty string to get the reversed string
+## NOTE - If you concatenate empty string to the var the string will not be reversed at all
+	reversed_string = rev + reversed_string 
+
+# Now print the final output
+print(reversed_string)
+
+## desired output - ereh )arorA vanhsiaV( 81ohcysp-re s'tI olleH
